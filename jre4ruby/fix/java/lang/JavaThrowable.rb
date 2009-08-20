0d9b925066a9852bcd370ccab747d8d5e6dd2294
neelance <mail@richard-musiol.de>
2009-08-20 15:37:06 +0200

    Fixed JavaThrowable#backtrace (quiet crash in Ruby 1.8).

--- jre4ruby/fix/java/lang/JavaThrowable.rb
+++ jre4ruby/fix/java/lang/JavaThrowable.rb
@@ -1,9 +1,13 @@
+class Exception
+  alias_method :ruby_backtrace, :backtrace
+end
+
 class Java::Lang::JavaThrowable
   def fill_in_stack_trace
     @stack_trace = caller
   end
 
   def backtrace
-    super
+    ruby_backtrace
   end
 end
