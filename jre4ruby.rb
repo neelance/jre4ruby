1e5dd364f9e98490c70cdb464a6e6553ea3808de
neelance <mail@richard-musiol.de>
2009-09-14 22:47:49 +0200

    New class loading.

--- jre4ruby.rb
+++ jre4ruby.rb
@@ -1,9 +1,5 @@
 require "rjava"
 
-lib_path = "#{File.dirname(__FILE__)}/jre4ruby/lib"
-fix_path = "#{File.dirname(__FILE__)}/jre4ruby/fix"
-rep_path = "#{File.dirname(__FILE__)}/jre4ruby/rep"
-
 platform = case RUBY_PLATFORM
 when "i386-mingw32"
   "windows"
@@ -11,25 +7,6 @@ else
   "solaris"
 end
 
-add_class_loader { |package_path|
-  dirs, names = list_paths "#{lib_path}/share/#{package_path}", "#{lib_path}/#{platform}/#{package_path}", "#{rep_path}/#{package_path}"
-  
-  dirs.each do |dir|
-    import_package dir, package_path
-  end
-  
-  names.each do |name|
-    file_path = "#{package_path}/#{name}.rb"
-    if File.exist?("#{rep_path}/#{file_path}")
-      import_class name, "jre4ruby/rep/#{file_path}"
-    elsif File.exist?("#{fix_path}/#{file_path}")
-      import_class name, "jre4ruby/lib/share/#{file_path}", "jre4ruby/fix/#{file_path}"
-    elsif File.exist?("#{lib_path}/share/#{file_path}")
-      import_class name, "jre4ruby/lib/share/#{file_path}"
-    elsif File.exist?("#{lib_path}/#{platform}/#{file_path}")
-      import_class name, "jre4ruby/lib/#{platform}/#{file_path}"
-    else
-      import_java_class name, "#{package_path.gsub("/", ".")}.#{name}"
-    end
-  end
-}
+add_class_path "jre4ruby/rep"
+add_class_path "jre4ruby/lib/share", "jre4ruby/fix"
+add_class_path "jre4ruby/lib/#{platform}", "jre4ruby/fix"
